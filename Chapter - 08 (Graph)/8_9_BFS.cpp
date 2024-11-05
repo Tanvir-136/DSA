5813c7c8638640bbd73a5a6e83baeaf2a943c511
Tanvir-136 <tanvir12islam34@gmail.com>
2024-11-05 08:29:50 +0600

    Update BFS

--- Chapter - 08 (Graph)/8_9_BFS.cpp
+++ Chapter - 08 (Graph)/8_9_BFS.cpp
@@ -6,37 +6,37 @@ vector<int> graph[N];
 bool vis[N];
 
 void bfs(int start){
-    queue<int> q;
+    queue<int>  q;
     q.push(start);
 
     while(!q.empty()){
         int cur = q.front();
         q.pop();
-        if (!vis[cur])
-            vis[cur] = true;
-            
+        if (vis[cur])
+            continue;
+        vis[cur] = true;
         cout << cur << ' ';
-        for(auto child: graph[cur]){
-            if(!vis[child]){
-                q.push(child);
-            }
+        for(auto it : graph[cur]){
+            if (vis[it])
+                continue;
+            q.push(it);
+            
         }
+
     }
 }
-int main() {
-    int n, e; 
-    cout << "Enter the number of nodes and edges: ";
-    cin >> n >> e;
-    cout << "Enter the edges (node1 node2):\n";
-    for (int i = 0; i < e; ++i) {
-        int u, v;
-        cin >> u >> v;
-        graph[u].push_back(v); 
-        graph[v].push_back(u); 
+int main(){
+    int node, edge;
+    cout << "Enter the number of vertex and edge: ";
+    cin >> node >> edge;
+    cout << "Enter the Number(v1, v2) of vertex which are connected by the edge: \n";
+    for (int i = 0; i < edge; ++i){
+        int v1 , v2;
+        cin >> v1 >> v2;
+        graph[v1].push_back(v2);
+        graph[v2].push_back(v1); // for undirected graph
     }
-
-    cout << "BFS Traversal from node 1: ";
-    bfs(1); 
-    cout << endl;
+    cout << " DFS traversal of this graph from vertex 1: ";
+    bfs(1);
     return 0;
 }
